84fb99601b5f8c68efd68f77f5b784862b911d4c
Hélio Filho <contatohelioseverino@gmail.com>
2020-02-23 14:25:22 -0300

    as

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Injectable({
     providedIn: 'root'
@@ -8,14 +9,24 @@ export class AppConfig {
     private darkMode: boolean = false;
     private fullScreen: boolean = false;
 
-    constructor() {
+    constructor(private nativeStorage: NativeStorage) {
 
-        if (this.darkMode) {
-
-
+        if (this.nativeStorage.getItem('dados')) {
+            this.nativeStorage.getItem('dados').then(
+                data => {
+                    console.log(data);
+                    this.darkMode = data.modo;
+                },
+                error => console.error(error)
+            );
         } else {
+            this.nativeStorage.setItem('dados', { modo: this.darkMode, fs: this.fullScreen })
+                .then(() => {
+                    console.log('Stored item!');
 
-
+                },
+                    error => console.error('Error storing item', error)
+                );
         }
 
     }
