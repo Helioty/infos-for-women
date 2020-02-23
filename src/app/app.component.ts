026c5fb5e8c7a66957734fba1a0f8985c9eb5060
Hélio Filho <contatohelioseverino@gmail.com>
2020-02-23 18:31:01 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, MenuController } from '@ionic/angular';
+import { Platform} from '@ionic/angular';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -16,7 +16,6 @@ export class AppComponent {
   private fullScreen: boolean = false;
 
   constructor(
-    private menu: MenuController,
     private platform: Platform,
     private nativeStorage: NativeStorage,
     private splashScreen: SplashScreen,
@@ -33,7 +32,6 @@ export class AppComponent {
         if (this.nativeStorage.getItem('dados')) {
           this.nativeStorage.getItem('dados').then(
             data => {
-              console.log(data);
               this.darkMode = data.modo;
               if (this.darkMode) {
                 this.statusBar.backgroundColorByHexString('#C40318');
@@ -44,10 +42,10 @@ export class AppComponent {
             error => console.error(error)
           );
         } else {
+          this.statusBar.backgroundColorByHexString('#ffbfd3');
           this.nativeStorage.setItem('dados', { modo: this.darkMode, fs: this.fullScreen })
             .then(() => {
               console.log('Stored item!');
-              this.statusBar.backgroundColorByHexString('#ffbfd3');
             },
               error => console.error('Error storing item', error)
             );
